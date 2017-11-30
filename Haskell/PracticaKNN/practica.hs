b9039a438f975c25f6f69574e59a6f7c8d6615b4
Alcasser <alcacer.la.1001@gmail.com>
2017-11-30 16:12:15 +0100

    small fixes

--- Haskell/PracticaKNN/practica.hs
+++ Haskell/PracticaKNN/practica.hs
@@ -1,17 +1,25 @@
+{--
+TODO:
+-Preguntar sobre la accuracy
+-Preguntar sobre como hacer la version ponderada
+-Repasar codigo
+-
+--}
+
 import System.IO
 import Data.List.Split -- Using it to comma separate words in each observation
 
 --Data constructor of a Flower
 type Label = String
-data Flower a = Flower Label [a] deriving(Eq, Show)
+data Flower = Flower Label [Double] deriving(Show)
 
-getLabelF :: Flower a -> Label
+getLabelF :: Flower -> Label
 getLabelF (Flower l _) = l
 
 --Data constructor of a distance between two observations and the corresponding Label
-data Dist a = Dist Label a deriving(Show)
+data Dist = Dist Label Double deriving(Show)
 
-getLabelD :: Dist a -> Label
+getLabelD :: Dist -> Label
 getLabelD (Dist l _) = l
 
 
@@ -52,36 +60,54 @@ Helper functions to calculate distance between observations,
 vote and perform the evalutaion
 --}
 
-instance Eq a => Eq (Dist a) where
+instance Eq Dist where
     (==) (Dist _ d1) (Dist _ d2) = d1 == d2
 
-instance (Eq a, Ord a) => Ord (Dist a) where
+instance Ord Dist where
     compare (Dist _ d1) (Dist _ d2) = compare d1 d2
 
 --Function to calculate the Euclidean distance between two observations
-euclideanDist :: Floating a => Flower a -> Flower a -> a
+euclideanDist :: Flower -> Flower -> Double
 euclideanDist (Flower _ as) (Flower _ bs) = sqrt $ sum $ map (^2) (zipWith (-) as bs)
 
 --Function to calculate the Manhattan distance between two observations
-manhattanDist :: Floating a => Flower a -> Flower a -> a
+manhattanDist :: Flower -> Flower -> Double
 manhattanDist (Flower _ as) (Flower _ bs) = sum $ map abs (zipWith (-) as bs)
 
---Function to calculate distances with the testing dataset given a flower
-calcDists :: Floating a => (Flower a -> Flower a -> a) -> [Flower a] -> Flower a -> [Dist a]
+--Function to calculate distances with the training dataset given a flower and a distance calculation function
+calcDists :: (Flower -> Flower -> Double) -> [Flower] -> Flower -> [Dist]
 calcDists df flowers newFlower = map calcDistf flowers
     where
         calcDistf = \flower -> Dist (getLabelF flower) (df newFlower flower)
 
 --Function to perform a basic vote
-basicVote :: [Dist a] -> Label
+basicVote :: [Dist] -> Label
 basicVote ds = head $ head $ filter ((freq ==) . length) groups
         where
             groups = group $ qsort (map getLabelD ds)
             freq = maxa $ map length groups 
 
+--Function to perform a weighted votation
+--weightedVote :: [Dist] -> Label
+--weightedVote ds = map \(Dist _ d) ds
+
+--Function to evaluate the accuracy of the classification
+accuracyEv :: [Label] -> [Label] -> Double
+accuracyEv predLabels testLabels = (fromIntegral $ length $ filter id $ zipWith (==) predLabels testLabels) / (fromIntegral $ length testLabels)
+
+--Function to evaluate the error of the classication
+errorEv ::[Label] -> [Label] -> Double
+errorEv predLabels testLabels = (fromIntegral $ length $ filter not . id $ zipWith (==) predLabels testLabels) / (fromIntegral $ length testLabels)
+
+
 --Function kNN to classify a new flower
-kNN :: (Ord a, Floating a) => [Flower a] -> (Flower a -> Flower a -> a) -> ([Dist a] -> Label) -> Int -> (Flower a -> Label)
+kNN :: [Flower] -> (Flower -> Flower -> Double) -> ([Dist] -> Label) -> Int -> (Flower -> Label)
 kNN flowers df vf k = vf . (take k) . qsort . (calcDists df flowers)
+
+--Function to evaluate a classification using a list of functions of evaluation
+evaluation :: [Label] -> [Label] -> [[Label] -> [Label] -> Double] -> [Double]
+evaluation predLabels testLabels fs = [f predLabels testLabels | f <- fs]
+
 
 {--
 Helper functions to parse data.
@@ -89,19 +115,19 @@ Using System.IO to read training and testing CSV files.
 --}
 
 --Function to create a flower given a string line containing this flower data
-parseFlower :: String -> Flower Double
+parseFlower :: String -> Flower
 parseFlower flower = Flower (last finfo) (map read $ init finfo)
         where
             finfo = splitOn "," flower
 
 --Function to create a data structure of Flowers
-parseI :: String -> [Flower Double]
+parseI :: String -> [Flower]
 parseI strOfFlowers = map parseFlower (lines strOfFlowers)
 
-performKnn :: [Flower Double] -> [Flower Double] -> [Label]
+performKnn :: [Flower] -> [Flower] -> [Label]
 performKnn fsTrain fsTest = map (kNN fsTrain euclideanDist basicVote 5) fsTest
 
-getLabels :: [Flower Double] -> [Label]
+getLabels :: [Flower] -> [Label]
 getLabels fs = map getLabelF fs
 
 --Main program execution
@@ -111,7 +137,11 @@ main = do
     handleTesting <- openFile "./iris.test.csv" ReadMode
     learningFlowers <- hGetContents handleLearning
     testingFlowers <- hGetContents handleTesting
+    let predLabs = performKnn (parseI learningFlowers) (parseI testingFlowers)
+    let realLabs = getLabels (parseI testingFlowers)
     putStrLn "Classified as: "
-    putStrLn $ show $ performKnn (parseI learningFlowers) (parseI testingFlowers)
+    mapM_ print predLabs
     putStrLn "Real labels: "
-    putStrLn $ show $ getLabels (parseI testingFlowers)
+    mapM_ print realLabs
+    putStr "Accuracy: "
+    print $ evaluation predLabs realLabs [accuracyEv, errorEv]
